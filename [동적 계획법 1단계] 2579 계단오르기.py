21ee8a6e556d2ec5561950445cf65f80bec47064
sinhyunsung <hyunsungsung@naver.com>
2020-01-25 00:10:40 +0900

    2020-01-25-01

--- [동적 계획법 1단계] 2579 계단오르기.py
+++ [동적 계획법 1단계] 2579 계단오르기.py
@@ -2,14 +2,18 @@ a=int(input())
 b=[]
 
 for i in range(a):
-    b.append[[int(input()),0,0,0]]
+    b.append([int(input()),0,0,0])
 
-for i in ragne(a):
+for i in range(a):
     if i==0:
-        b[i][1],b[i][2],b[i][3]=b[i][0],b[i][0],b[i][0]
+        b[i][1],b[i][2],b[i][3]=b[i][0],0,0
     elif i==1:
-        b[i][1]=0
-        b[i][2]=b[i-1][2]+b[i][0]
-        b[i][3]=b[i-1][3]
+        b[i][1]=b[i][0]
+        b[i][2]=b[i-1][1]+b[i][0]
+        b[i][3]=0
     else:
-        b[i][1]=max(b[i-2])
+        b[i][1]=max(b[i-2][1]+b[i][0],b[i-2][2]+b[i][0])
+        b[i][2]=b[i-1][1]+b[i][0]
+        b[i][3]=max(b[i-1][1],b[i-1][2])
+
+print(max(b[a-1][1],b[a-1][2]))
